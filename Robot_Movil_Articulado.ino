4df180dfedeadc58e31099063735c37115533021
rafmpuent <107746349+rafmpuent@users.noreply.github.com>
2025-08-04 02:33:09 -0500

    Update Robot_Movil_Articulado.ino

--- Robot_Movil_Articulado.ino
+++ Robot_Movil_Articulado.ino
@@ -15,11 +15,24 @@ int leftMotorPin1=27;
 int leftMotorPin2=14;
 
 //Servo
-Servo servoEE, servoE2, servoE1, servoBase;
+Servo servoEE;
+Servo servoE2;
+Servo servoE1;
+Servo servoBase;
+
+// Pinout Servo
+int servoEEPin = 15;
+int servoE2Pin = 16;
+int servoE1Pin = 14;
+int servoBasePin = 14;
+
+// Published values for SG90 servos
+int minUs = 500;
+int maxUs = 2500;
 
 #define MAX_MOTOR_SPEED 255
 
-const int PWMFreq = 1000; /* 1 KHz */
+const int PWMFreq = 5000; /* 5 KHz */
 const int PWMResolution = 8;
 const int rightMotorPWMSpeedChannel = 4;
 const int leftMotorPWMSpeedChannel = 5;
@@ -62,13 +75,24 @@ void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
   ledcWrite(leftMotorPWMSpeedChannel, abs(leftMotorSpeed));  
 }
 
-void rotationArm(int angle){
-  if (angle > 0){
-    
+void moveArm(int positions){
+  int oldValue, newValue, reader;
+  oldValue = servoE1.read();
+  reader = servoE2.read();
+
+  servoE1.write(positions);
+  newValue = servoE1.read();
+
+  if (oldValue < newValue){
+    servoE2.write(reader + 45/2);
   }
   else{
-
+    servoE2.write(reader - 45/2);
   }
+}
+
+void rotationArm(int angle){
+  servoBase.write(angle);
 }
 
 void setUpPinModes()
@@ -89,26 +113,27 @@ void setUpPinModes()
 
   rotateMotor(0,0);
 
-  servoEE.attach(23, 500, 2500);
-  servoEE.setPeriodHertz(50);
+  Serial.begin(115200);
 
-  servoE1.attach(22, 500, 2500);
-  servoE1.setPeriodHertz(50);
-  
-  servoE2.attach(21, 500, 2500);
+  servoEE.setPeriodHertz(50);
   servoE2.setPeriodHertz(50);
-  
-  servoBase.attach(19, 500, 2500);
+  servoE1.setPeriodHertz(50);
   servoBase.setPeriodHertz(50);
 
-  //Position Initial
-  servoEE.writeMicroseconds(500);
-  servoE1.writeMicroseconds(500);
-  servoE2.writeMicroseconds(500);
-  servoBase.writeMicroseconds(500);
-
+  servoEE.attach(servoEEPin, minUs, maxUs);
+  servoE2.attach(servoE2Pin, minUs, maxUs);
+  servoE1.attach(servoE1Pin, minUs, maxUs);
+  servoBase.attach(servoBasePin, minUs, maxUs);
+  
   bool flagSelection = false;
-  int rotation = 0;
+  int rotation = 90;
+  int position = 90
+
+  //Position Initial
+  servoEE.write(0);
+  servoE2.write(0);
+  servoE1.write(90);
+  seroBase.write(rotation);
 }
 
 void setup()
@@ -124,26 +149,26 @@ void loop()
   Dabble.processInput();
   if (GamePad.isUpPressed())
   {
-    rightMotorSpeed = MAX_MOTOR_SPEED;
+    rightMotorSpeed = -MAX_MOTOR_SPEED;
     leftMotorSpeed = MAX_MOTOR_SPEED;
   }
 
   if (GamePad.isDownPressed())
   {
-    rightMotorSpeed = -MAX_MOTOR_SPEED;
+    rightMotorSpeed = MAX_MOTOR_SPEED;
     leftMotorSpeed = -MAX_MOTOR_SPEED;
   }
 
   if (GamePad.isLeftPressed())
   {
     rightMotorSpeed = MAX_MOTOR_SPEED;
-    leftMotorSpeed = -MAX_MOTOR_SPEED;
+    leftMotorSpeed = MAX_MOTOR_SPEED;
   }
 
   if (GamePad.isRightPressed())
   {
     rightMotorSpeed = -MAX_MOTOR_SPEED;
-    leftMotorSpeed = MAX_MOTOR_SPEED;
+    leftMotorSpeed = -MAX_MOTOR_SPEED;
   }
 
   rotateMotor(rightMotorSpeed, leftMotorSpeed);
@@ -152,34 +177,55 @@ void loop()
   {
     if (!flagSelection)
     {
-      servoEE.writeMicroseconds(2500);
+      servoEE.write(135);
     }
     else
     {
-      servoEE.writeMicroseconds(500);
+      servoEE.write(0);
     }
     flagSelection = !flagSelection;  
   }
 
+  reader = servoE2.read();
+
   if (GamePad.isTrianglePressed()) //Forward Arm
   {
-    moveArm();
-  }
-
-  if (GamePad.isCirclePressed()) //Rotation Rigth
-  {
-    rotation += 45;
+    if (positionE>0){
+      positionE -= 45;
+    }
+    else{
+      if (reader>0){
+        servoE2.write(reader - 45);
+      }
+    }
   }
 
   if (GamePad.isCrossPressed()) //Backward Arm
   {
-    moveArm(); 
+    if (positionE<180){
+      positionE += 45;
+    }
+    else{
+      if (reader<180){
+        servoE2.write(reader + 45);
+      }
+    }
+  }
+
+  if (GamePad.isCirclePressed()) //Rotation Rigth
+  {
+    if (rotation<180){
+      rotation += 10;
+    }
   }
 
   if (GamePad.isSquarePressed()) //Rotation Left
   {
-    rotation -= 45; 
+    if (rotation>0){
+      rotation -= 10;
+    } 
   }
 
+  moveArm(positionE);
   rotationArm(rotation);
 }
